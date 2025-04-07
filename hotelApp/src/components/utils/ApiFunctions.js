eb32fae048f057a9ab458c6258b7f2bb5a11a207
Anthony <anthonypanont@gmail.com>
2025-04-07 16:15:40 +0200

    Pages : EditRoom / AddRoom -> function UpdateRoom

--- hotelApp/src/components/utils/ApiFunctions.js
+++ hotelApp/src/components/utils/ApiFunctions.js
@@ -59,3 +59,33 @@ export async function deleteRoom(roomId){
         throw new Error(`Error deleting room ${error.message}`)
     }
 }
+
+/** 
+ * Update Room
+ * 
+ * on l'a fetch dabord et ensuite on edit au besoin
+ **/
+export async function updateRoom(roomId, roomData){
+    const formData = new FormData();
+    formData.append("roomType", roomData.roomType)
+    formData.append("roomPrice", roomData.roomPrice)
+    formData.append("photo", roomData.photo)
+    
+    const response = await api.put(`/rooms/update/${roomId}`, formData)
+    return response
+}
+
+/**
+ * Get room by id
+ * 
+ * @param {*} roomId 
+ * @returns 
+ */
+export async function getRoomById(roomId){
+    try {
+        const result = await api.get(`/rooms/room/${roomId}`)
+        return result.data
+    } catch (error) {
+        throw new Error(`Error fetching room : ${error.message}`)
+    }
+}
