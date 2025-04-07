fbc9903a526cf6836ef7dd52b0193da56f37529c
Anthony <anthonypanont@gmail.com>
2025-04-07 19:37:55 +0200

    RoomListing(App.jsx) -> Room -> RoomCard

--- hotelApp/src/App.jsx
+++ hotelApp/src/App.jsx
@@ -11,6 +11,7 @@ import Home from "./components/home/Home.jsx";
 import EditRoom from "./components/room/EditRoom.jsx";
 import NavBar from "./components/layout/NavBar.jsx";
 import Footer from "./components/layout/Footer.jsx";
+import RoomListing from "./components/room/RoomListing.jsx";
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
             <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
           </Routes>
         </Router>
         <Footer />
